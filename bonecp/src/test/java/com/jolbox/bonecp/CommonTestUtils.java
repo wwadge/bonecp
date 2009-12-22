692f499d127090449fb7e1fafb9ed04ea7423214
wallacew <wallacew@mtpc-wallacew.malta.ccbill-hq.local>
2009-12-22 14:32:58 +0100

    GPL copyright stuff

--- bonecp/src/test/java/com/jolbox/bonecp/CommonTestUtils.java
+++ bonecp/src/test/java/com/jolbox/bonecp/CommonTestUtils.java
@@ -1,3 +1,23 @@
+/*
+
+Copyright 2009 Wallace Wadge
+
+This file is part of BoneCP.
+
+BoneCP is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+BoneCP is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BoneCP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.jolbox.bonecp;
 
 import static org.easymock.EasyMock.expect;
