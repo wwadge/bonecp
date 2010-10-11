8cdad6e065bcf9abc5bfee42119f7e61446b4920
Wallace <wwadge@gmail.com>
2010-10-11 14:39:07 +0200

    Cleanups

--- bonecp/src/test/java/com/jolbox/bonecp/TestConnectionMaxAgeTester.java
+++ bonecp/src/test/java/com/jolbox/bonecp/TestConnectionMaxAgeTester.java
@@ -14,45 +14,16 @@
  *    limitations under the License.
  */
 
-/*
-
-Copyright 2009 Wallace Wadge
-
-This file is part of BoneCP.
-
-BoneCP is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-BoneCP is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with BoneCP.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 package com.jolbox.bonecp;
 
-import static org.easymock.EasyMock.anyLong;
-import static org.easymock.EasyMock.anyObject;
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.expectLastCall;
 import static org.easymock.classextension.EasyMock.createNiceMock;
 import static org.easymock.classextension.EasyMock.makeThreadSafe;
-import static org.easymock.classextension.EasyMock.replay;
 import static org.easymock.classextension.EasyMock.reset;
-import static org.easymock.classextension.EasyMock.verify;
 
-import java.lang.reflect.Field;
-import java.sql.SQLException;
 import java.util.concurrent.ScheduledExecutorService;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Test;
 import org.slf4j.Logger;
 
 /**
